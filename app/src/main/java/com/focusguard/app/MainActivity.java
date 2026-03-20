d09cb617466b90d85b4b6ba7b8ce147038ffb784
priyaroyp28-star <priyaroyp28@gmail.com>
2026-03-21 00:31:14 +0600

    Add MainActivity class with onCreate method

---
+++ app/src/main/java/com/focusguard/app/MainActivity.java
@@ -0,0 +1,14 @@
+package com.focusguard.app;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // এটি আপনার তৈরি করা ডিজাইনটিকে স্ক্রিনে যুক্ত করবে
+        setContentView(R.layout.activity_main);
+    }
+}
